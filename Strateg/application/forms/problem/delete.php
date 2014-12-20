c05787d0f28cd1f0b744ff5d2187fe4f154c7ddb
Alcara11 <sokolova.jana11@gmail.com>
2014-12-20 20:54:50 +0100

    message

--- Strateg/application/forms/problem/delete.php
+++ Strateg/application/forms/problem/delete.php
@@ -4,8 +4,7 @@ class Application_Form_Problem_Delete extends Zend_Form {
 
     public function init() {
         $configFilePath = APPLICATION_PATH . "/forms/problem/configs/delete.ini";
-        $config = new Zend_Config_Ini($configFilePath);
-        
+        $config = new Zend_Config_Ini($configFilePath);        
         $this->setConfig($config);
     }
 
