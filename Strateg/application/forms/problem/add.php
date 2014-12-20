15fa1a3f04096098592b07a7d2fde980d010f714
Simoniel <j4nkas@gmail.com>
2014-12-20 20:47:40 +0100

    hlaska

--- Strateg/application/forms/problem/add.php
+++ Strateg/application/forms/problem/add.php
@@ -4,8 +4,7 @@ class Application_Form_Problem_Add extends Zend_Form {
 
     public function init() {
         $configFilePath = APPLICATION_PATH . "/forms/problem/configs/add.ini";
-        $config = new Zend_Config_Ini($configFilePath);
-        
+        $config = new Zend_Config_Ini($configFilePath);        
         $this->setConfig($config);
     }
 
