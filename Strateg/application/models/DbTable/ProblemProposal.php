6d3c3aa427d422f8c0c8253e0517ffaec1fff952
Alcara11 <sokolova.jana11@gmail.com>
2014-12-21 12:53:20 +0100

    PN db model

--- Strateg/application/models/DbTable/ProblemProposal.php
+++ Strateg/application/models/DbTable/ProblemProposal.php
@@ -4,14 +4,16 @@ class Application_Model_DbTable_ProblemProposal extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'problem_navrh';
-    protected $_primary = 'id';
+    protected $_primary = array('id_problem', 'id_analyza');
 
-    public function getProblemProposal($id)
+    public function getProblemProposal($id_problem, $id_navrh)
     {
-        $id = (int)$id;
-        $row = $this->fetchRow('id = ' . $id);
+        $id_problem = (int)$id_problem;
+        $id_navrh = (int)$id_navrh;
+        $row = $this->fetchRow('id_problem = ' . $id_problem . 
+                ' and id_navrh = '. $id_navrh);
         if (!$row) {
-            throw new Exception("Could not find row $id");
+            throw new Exception("Could not find row $id_problem $id_navrh");
         }
         return $row->toArray();
     }
@@ -20,13 +22,23 @@ class Application_Model_DbTable_ProblemProposal extends Zend_Db_Table_Abstract
         $this->insert($data);
     }
 
-    public function updateProblemProposal($id, $data) {
+    public function updateProblemProposal($id_problem, $id_navrh, $data) {
         file_put_contents('test', $data);
-        $this->update($data, 'id = '. (int)$id);
+        $this->update($data, 'id_problem = ' . (int)$id_problem .
+                ' and id_navrh = '. (int)$id_navrh);
     }
 
-    public function deleteProblemProposal($id) {
-        $this->delete('id = ' . (int)$id);
+    public function deleteProblemProposal($id_problem, $id_navrh) {
+        $this->delete('id_problem = ' . (int)$id_problem . 
+                ' and id_navrh = '. (int)$id_navrh);
+    }
+    
+    public function deletePPbyProblem($id_problem) {
+        $this->delete('id_problem = ' . (int)$id_problem);
+    }
+    
+    public function deletePPbyProposal($id_navrh) {
+        $this->delete('id_navrh = '. (int)$id_navrh);
     }
 
 }
